786d878bf6b6efa0c2ff2d0a0579fa65cd057f13
fccc2aa3e29 <49536873+fccc2aa3e29@users.noreply.github.com>
2019-05-25 06:27:07 +0000

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,14 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const stockHandler = require('../controllers/stockHandler.js');
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
-    });
+    stockHandler(req, res)  
+  });
     
 };
+
